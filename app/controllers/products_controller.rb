7ca39fe9d761afc43a0fe022dfb96314c9ffecf0
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-24 16:42:54 -0500

    Implemented getScript

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,9 @@ class ProductsController < ApplicationController
       Product.all
     end
 
-    if request.xhr?
-      render partial: "shared/product", collection: @products
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
